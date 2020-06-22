3136ee42d28a8d1deeac50a2ad826dc7f058da03
mcgithu <529672848@qq.com>
2020-06-22 10:09:36 +0800

    新增topic消息

--- activemqdemo/src/main/java/com/activemq/demo/controller/MqController.java
+++ activemqdemo/src/main/java/com/activemq/demo/controller/MqController.java
@@ -1,21 +1,29 @@
 package com.activemq.demo.controller;
 
 import com.activemq.demo.producer.Producer;
+import com.activemq.demo.producer.TopicProducer;
 import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.jms.Destination;
+import javax.jms.JMSException;
 
 @RestController
 @RequestMapping("/mq")
 public class MqController {
 
+
+    private final Destination DEFAULT_QUEUE = new ActiveMQQueue("springboot.queue");
+
+//    private final Destination TOPIC_NAME = new ActiveMQTopic("springboot.topic");
     @Autowired
     private Producer producer;
-    private final Destination DEFAULT_QUEUE = new ActiveMQQueue("springboot.queue");
+    @Autowired
+    private TopicProducer topicProducer;
 
     /**
      * 普通的消息
@@ -32,5 +40,13 @@ public class MqController {
     @RequestMapping("/sendDeleyMsg")
     public void sendDeleyMsg(String msg){
         producer.delaySend(DEFAULT_QUEUE,msg,10*1000L);
+    }
+
+    /**
+     * 主题订阅模式消息
+     */
+    @RequestMapping("/sendTopicMsg")
+    public void sendTopicMsg(String msg) throws JMSException {
+        topicProducer.producer(msg);
     }
 }
